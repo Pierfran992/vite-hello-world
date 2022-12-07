4796c41e7d88e49fbba923ff1735c3fe27d5c493
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-07 17:32:04 +0100

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return {
+            imgHero: "../public/img/heroimg.png"
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <img :src="imgHero" alt="">
+    </div>
+</template>
+
+<style scoped>
+.container {
+    width: 60%;
+    margin: 0 auto;
+}
+
+img {
+    width: 100%;
+    box-shadow: 0px 0px 10px #ffd32b, 0px 2px 10px #ffd32b;
+    border-radius: 20px;
+    transform: perspective(1500px) rotateY(15deg);
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    data() {
+        return {
+            title1: "La mia prima ",
+            title2: "app con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>
+        <span class="title1">{{ title1 }}</span>
+        <span class="title2">{{ title2 }}</span>
+    </h1>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+
+h1 {
+    font-family: 'Pacifico', cursive;
+
+}
+
+.title1 {
+    color: #9961fe;
+}
+
+.title2 {
+    color: #41b883;
+}
+</style>
